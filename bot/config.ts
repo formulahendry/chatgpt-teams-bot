f21dc9ffb378ba55a9b616853f9f9f58d4d8658f
icorpsakurudi <124074580+icorpsakurudi@users.noreply.github.com>
2023-01-31 11:33:16 -0500

    First Branch
    New Branch

--- bot/config.ts
+++
@@ -1,9 +0,0 @@
-const config = {
-  botId: process.env.BOT_ID,
-  botPassword: process.env.BOT_PASSWORD,
-  chatgptSessionToken: process.env.CHATGPT_SESSION_TOKEN,
-  clearanceToken: process.env.CLEARANCE_TOKEN,
-  userAgent: process.env.USER_AGENT,
-};
-
-export default config;
